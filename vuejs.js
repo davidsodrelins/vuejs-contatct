0cd01f507e1bb2786caaae55ae9c6fec17ff6a34
David Sodré <davidsodre_ba@hotmail.com>
2019-06-06 13:12:43 -0300

    Estruturação do contato e utilização das diretivas v-if e v-else. | Contact estruturation and utilization od directives v-if and v-else

--- vuejs.js
+++ vuejs.js
@@ -1,7 +1,15 @@
-
 var app = new Vue({
     el: "#app",
     data: {
-        titulo: "Aplicação com VueJS 3!"
+        title: "Contacts VueJS",
+        message_error: "Contact not found.",
+        message_success: "Found!",
+        contact: {
+            id: 1,
+            name: "David Sodre Lins",
+            phone: "+5571992035786",
+            email: "davidsodre@outlook.com.br"
+        }
+        
     }
 });
